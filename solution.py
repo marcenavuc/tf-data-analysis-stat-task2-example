759ff4216717f7dc3fa666385b18b091607a316b
marcenavuc <mark.averchenko@gmail.com>
2023-04-01 14:04:52 +0500

    attempt 1

--- solution.py
+++ solution.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import norm, chi2
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 351730666 # Ваш chat ID, не меняйте название переменной
+
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    n = len(x)
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    s2 = np.var(x, ddof=1)
+    left = (n - 1) * s2 / chi2.ppf(1 - alpha / 2, df=n - 1)
+    right = (n - 1) * s2 / chi2.ppf(alpha / 2, df=n - 1)
+    return np.sqrt(left / 45), np.sqrt(right / 45)
+
