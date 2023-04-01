c4c99b51e3d45f14dbcde8e0e237a965d707707a
marcenavuc <mark.averchenko@gmail.com>
2023-04-01 16:50:27 +0500

    attempt 2

--- solution.py
+++ solution.py
@@ -13,5 +13,5 @@ def solution(p: float, x: np.array) -> tuple:
     s2 = np.var(x, ddof=1)
     left = (n - 1) * s2 / chi2.ppf(1 - alpha / 2, df=n - 1)
     right = (n - 1) * s2 / chi2.ppf(alpha / 2, df=n - 1)
-    return np.sqrt(left / 45), np.sqrt(right / 45)
+    return left, right
 
