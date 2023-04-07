409221eaed3b78250ff7bb3accf2304950f1af6f
marcenavuc <mark.averchenko@gmail.com>
2023-04-07 23:54:56 +0500

    god please 4

--- solution.py
+++ solution.py
@@ -7,9 +7,21 @@ from scipy.stats import norm, chi2
 chat_id = 351730666 # Ваш chat ID, не меняйте название переменной
 
 
+
 def solution(p: float, x: np.array) -> tuple:
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    alpha = 1 - p
     n = len(x)
-    s2 = np.var(x, ddof=1)
-    left = np.sqrt(n - 1) * s2 / chi2.ppf((1 - p) / 2, df=n - 1)
-    right = np.sqrt(n - 1) * s2 / chi2.ppf((1 + p) / 2, df=n - 1)
-    return left, right
+    
+    x2 = np.array([xi**2 for xi in x])
+    x2_mean = x2.mean()
+    
+    chi2_rv = chi2(df = 2 * n)
+    
+    left = chi2_rv.ppf(1 - alpha / 2) #b
+    right = chi2_rv.ppf(alpha / 2) #a
+    
+    return np.sqrt(n * x2_mean / (left * 2)), np.sqrt(n * x2_mean / (right * 2))
+    
